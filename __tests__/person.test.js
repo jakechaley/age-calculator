65293f535e95c42fa723c5bbcc1c8031233aabf1
jakechaley <jakechaley@gmail.com>
2022-02-05 16:09:37 -0800

    get good fail for yearsPastLifeExpectancyOnEarth test

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -4,44 +4,59 @@ describe('Person', () => {
   let person1;
 
   beforeEach(() => {
-    person1 = new Person(26);
+    person1 = new Person(26, 73);
     
   });
+
   test('should create a person object with age constructor', () => {
     expect(person1.age).toEqual(26);
   });
+
   test('should return persons age on earth', () => {
     expect(person1.earthAge()).toEqual(26);
   });
+
   test('should return persons age on mercury', () => {
     expect(person1.mercuryAge()).toEqual(108);
   });
+
   test('should return persons age on venus', () => {
     expect(person1.venusAge()).toEqual(42);
   });
+
   test('should return persons age on mars', () => {
     expect(person1.marsAge()).toEqual(14);
   });
+
   test('should return persons age on jupiter', () => {
     expect(person1.jupiterAge()).toEqual(2);
   });
-  test('should return life expectancy on earth', () => {
-    expect(person1.averageLifeExpectancy()).toEqual(73);
-  });
+
   test('should return how many years user has left to live on earth', () => {
     expect(person1.yearsLeftOnEarth()).toEqual(47);
   });
+
   test('should return how many years user has left to live on mercury', () => {
-    const person2 = new Person(2)
+    const person2 = new Person(2, 73)
     expect(person2.yearsLeftOnMercury()).toEqual(65);
   });
+
   test('should return how many years user has left to live on venus', () => {
     expect(person1.yearsLeftOnVenus()).toEqual(31)
   });
+
   test('should return how many years user has left to live on mars', () => {
     expect(person1.yearsLeftOnMars()).toEqual(59)
   });
+
   test('should return how many years user has left to live on jupiter', () => {
     expect(person1.yearsLeftOnJupiter()).toEqual(71)
   });
+
+  test('should return years lived past life expectancy on earth', () => {
+    const person3 = new Person(80, 73);
+    expect(person3.yearsPastLifeExpectancyOnEarth()).toEqual(7);
+  });
+  
+
 });
--- src/person.js
+++ src/person.js
@@ -1,50 +1,58 @@
 export default class Person {
-  constructor(age) {
+  constructor(age, lifeExpectancy) {
     this.age = age;
-    this.lifeExpectancy = 73;
+    this.lifeExpectancy = lifeExpectancy;
   }
 
   earthAge() {
     return this.age;
   }
+
   mercuryAge() {
     const mercuryAge = parseFloat((this.age / .24).toFixed());
     return mercuryAge;
   }
+
   venusAge() {
     const venusAge = parseFloat((this.age / .62).toFixed());
     return venusAge;
   }
+
   marsAge() {
-  const marsAge = parseFloat((this.age / 1.88).toFixed());
-  return marsAge;
+    const marsAge = parseFloat((this.age / 1.88).toFixed());
+    return marsAge;
   }
   jupiterAge() {
-  const jupiterAge = parseFloat((this.age / 11.86).toFixed());
-  return jupiterAge;
+    const jupiterAge = parseFloat((this.age / 11.86).toFixed());
+    return jupiterAge;
   }
-  averageLifeExpectancy() {
-  const averageLifeExpectancy = 73; //average life expectancy for humans rounded to a fixed point.
-  return averageLifeExpectancy;
-  }
+
   yearsLeftOnEarth() {
-  const yearsLeftOnEarth = parseFloat((this.lifeExpectancy - this.age).toFixed());
-  return yearsLeftOnEarth;
+    const yearsLeftOnEarth = parseFloat((this.lifeExpectancy - this.age).toFixed());
+    return yearsLeftOnEarth;
   }
+
   yearsLeftOnMercury() {
     const yearsLeftOnMercury = parseFloat((this.lifeExpectancy - this.mercuryAge()).toFixed());
     return yearsLeftOnMercury;
   }
+
   yearsLeftOnVenus() {
     const yearsLeftOnVenus = parseFloat((this.lifeExpectancy - this.venusAge()).toFixed());
     return yearsLeftOnVenus;
   }
+
   yearsLeftOnMars() {
     const yearsLeftOnMars = parseFloat((this.lifeExpectancy - this.marsAge()).toFixed());
     return yearsLeftOnMars;
   }
+
   yearsLeftOnJupiter() {
     const yearsLeftOnJupiter = parseFloat((this.lifeExpectancy - this.jupiterAge()).toFixed());
     return yearsLeftOnJupiter;
+  }
+
+  yearsPastLifeExpectancyOnEarth() {
+    
   }
 }
